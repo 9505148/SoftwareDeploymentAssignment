74336dac6f00556112d22fca2a2f2b7d993a1e6c
Aequitas737 <dgrani@hotmail.com>
2016-09-28 12:46:53 +1000

    Add files via upload
    uploaded filke

--- src/main/java/servlet/MyServlet.java
+++ src/main/java/servlet/MyServlet.java
@@ -14,14 +14,23 @@ import javax.servlet.http.HttpServletResponse;
         urlPatterns = {"/status"}
     )
 public class MyServlet extends HttpServlet {
-
+/*
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write(req.getParameter("age").getBytes());
+        out.write("IF YOU ARE SEEING THIS THEN IT IS WORKING".getBytes());
+        out.flush();
+        out.close();
+    }*/
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException {
+
+        ServletOutputStream out = resp.getOutputStream();
+        out.write(out.write(request.getParameter("age").getBytes());
         out.flush();
         out.close();
     }
+
     
 }
