73f30b58c0390705fb18e433af992fcd23b8803f
Aequitas737 <dgrani@hotmail.com>
2016-09-28 15:00:44 +1000

    Added missing imports

--- src/main/java/servlet/MyServlet.java
+++ src/main/java/servlet/MyServlet.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @WebServlet(
         name = "MyServlet", 
         urlPatterns = {"/status"}
