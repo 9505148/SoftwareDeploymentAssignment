270ebeaa5f9bea316f840a45d4f3c1b917b7f2d6
Aequitas737 <dgrani@hotmail.com>
2016-09-28 13:32:37 +1000

    Add files via upload
    Added connect to database, create table and add table.

--- src/main/java/servlet/MyServlet.java
+++ src/main/java/servlet/MyServlet.java
@@ -30,13 +30,57 @@ public class MyServlet extends HttpServlet {
         
         String userAge = request.getParameter("age");
         String userName = request.getParameter("name");
-        
-        
+        if(userAge == "1")
+        {
+        	get("/db", (request, response) ->  
+        {
+            Connection connection = null;
+            Map<String, Object> attributes = new HashMap<>();
+            try {
+              connection = DatabaseUrl.extract().getConnection();
+              Statement stmt = connection.createStatement();
+              stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+              stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+              ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+              ArrayList<String> output = new ArrayList<String>();
+              while (rs.next()) {
+                output.add( "Read from DB: " + rs.getTimestamp("tick"));
+              }
+
+               attributes.put("results", output);
+               return new ModelAndView(attributes, "db.ftl");
+             } catch (Exception e) {
+               attributes.put("message", "There was an error: " + e);
+               return new ModelAndView(attributes, "error.ftl");
+             } finally {
+               if (connection != null) try{connection.close();} catch(SQLException e){}
+             }
+           } ,new FreeMarkerEngine());
+        }
         
         out.write(request.getParameter("age").getBytes());
         out.flush();
         out.close();
     }
+/**
+ * postgres://qkzwskiqlsnorc:9IvEOeX1QUiVZPhAWWXelQz9s7@ec2-54-243-208-195.compute-1.amazonaws.com:5432/df1609d1j6jvn9
+ * password, 9IvEOeX1QUiVZPhAWWXelQz9s7
+ * user, qkzwskiqlsnorc
+ * @return
+ * @throws URISyntaxException
+ * @throws SQLException
+ */
+    /*
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-    
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+    */
+
 }
